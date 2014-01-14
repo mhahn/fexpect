462730e5cefb6f468b848db12404b827767edfd3
Michael Hahn <mhahn@eventbrite.com>
2014-01-13 19:54:10 -0800

    Some quick pep-8 clenaup

--- fexpect/api.py
+++ fexpect/api.py
@@ -1,7 +1,7 @@
 import fabric.api
 from .internals import (
-    wrapExpectations,
-    wrapExpectationsLocal,
+    wrap_expectations,
+    wrap_expectations_local,
     ExpectationContext,
 )
 
@@ -25,36 +25,45 @@ def expect(prompt, response, exit_after=None, hide_input=False):
         }
     ]
 
+
 def expecting(e):
     return ExpectationContext(e)
 
+
 def run(cmd, **kwargs):
-    #run wrapper 
+    """Wrapper around fabric run"""
     tmp_file = None
-    if 'expectations' in fabric.state.env and \
-        len(fabric.state.env.expectations) > 0:
-        cmd, tmp_file = wrapExpectations(cmd)
+    if (
+        'expectations' in fabric.state.env and
+        len(fabric.state.env.expectations) > 0
+    ):
+        cmd, tmp_file = wrap_expectations(cmd)
     result = fabric.api.run(cmd, **kwargs)
     if tmp_file:
         fabric.api.run('rm %s' % (tmp_file,))
     return result
 
+
 def sudo(cmd, **kwargs):
-    #sudo wrapper
+    """Wrapper around fabric sudo"""
     tmp_file = None
-    if 'expectations' in fabric.state.env and \
-        len(fabric.state.env.expectations) > 0:
-        cmd, tmp_file = wrapExpectations(cmd)
+    if (
+        'expectations' in fabric.state.env and
+        len(fabric.state.env.expectations) > 0
+    ):
+        cmd, tmp_file = wrap_expectations(cmd)
 
     result = fabric.api.sudo(cmd, **kwargs)
     if tmp_file:
         fabric.api.sudo('rm %s' % (tmp_file,))
     return result
 
-def local(cmd, **kwargs):
-    #local wrapper
-    if 'expectations' in fabric.state.env and \
-        len(fabric.state.env.expectations) > 0:
-        cmd = wrapExpectationsLocal(cmd)
-    return fabric.api.local(cmd, **kwargs)
 
+def local(cmd, **kwargs):
+    """Wrapper around fabric local"""
+    if (
+        'expectations' in fabric.state.env and
+        len(fabric.state.env.expectations) > 0
+    ):
+        cmd = wrap_expectations_local(cmd)
+    return fabric.api.local(cmd, **kwargs)
